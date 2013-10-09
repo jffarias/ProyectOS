34b72448d0c0edfd9e7ce84fbf44c5b3a6c0c38d
jesus <jesusflr4@gmail.com>
2013-10-09 18:52:24 -0500

    intercalado de archivos

--- simulador.c
+++ simulador.c
@@ -8,45 +8,114 @@
 
 int eax=0,ebx=0,ecx=0,edx=0,pc= 0;
 char auxIR[15];	
-int activo;
-void imprime();
-int kbhit(void);
-int dispatcher();
 char nombreArchivo[50];
-int estado;
+char auxarchi[15];
 int pid=1;
 
-struct pcb{
-int pid;
-int estado;
-int pc;
-int eax,ebx,ecx,edx;
+void dispatcher();
+int deletePID(int pid);
+/*void ejecucion(char archi[],int pid);*/
+void imprime();
+void imprimePCB(char archi[],int pidpcb);
+void insertFinal(char *nameArchivo,int PID);
+int kbhit(void);
+int moveFinal(int pidmover);
+void MuestraArch();
+int proceso(char archi[],int pid);
+void readArch();
+void saveContext();
+void status(int edo);
 
-struct pcb *siguiente;
-}PCB;
-
-struct Nodolistos
-{
-<<<<<<< HEAD
+/*estructuras para colas*/
+struct Nodolistos{
 	/* data */
 	int actualPID;
-	char nombreArchivo[50];
-
+	char nombreArchivo[15];
+	char estado[15];
+	char nombreArch[15];
+	int pc;
+	char auxIR[15];
+	int eax,ebx,ecx,edx;
+	//FILE *arch;
 	struct Nodolistos *siguiente;
-=======
-	int actual
-	struct listos *siguiente;
-};
-struct listos *list;
-
-struct ejecucion
-{
->>>>>>> 38bd80056778b6597828aedb5a9e351ca34179a8
 };
 struct Nodolistos *iniciolistos;
 
+
+/*+++++++++++++++++++++++++++++*/
+
+/*++++FUNCIONES++++*/
+
+void dispatcher(){
+	struct Nodolistos *temp;
+	temp=iniciolistos;
+	if(temp==NULL){
+		printf("Lista de listos VACIA\n");
+	}else{
+		proceso(temp->nombreArchivo,temp->actualPID);
+	}
+}
+
+int deletePID(int pid){
+	struct Nodolistos *temp, *anterior;
+	int encontrado=0;
+
+	if(iniciolistos==NULL){
+		return encontrado;
+	}
+	temp=iniciolistos;
+	anterior=NULL;
+	while(temp!=NULL && encontrado==0){
+		if(temp->actualPID==pid){
+			encontrado=1;
+			if(anterior==NULL){
+				iniciolistos=temp->siguiente;
+			}else{
+				anterior->siguiente=temp->siguiente;
+			}
+			free(temp);
+		}else{
+			anterior=temp;
+			temp=temp->siguiente;
+		}
+	}
+	return encontrado;
+}
+
+void imprime(int pc){
+	
+	initscr();
+
+	mvprintw(3,2,"------------------------CPU------------------------");
+	mvprintw(4,2,"PC\tIR\t\tEAX\tEBX\tECX\tEDX\n");
+
+	refresh();mvprintw(5,2,"%d",pc);refresh();
+	refresh();mvprintw(5,8,"%s      ",auxIR);
+	refresh();mvprintw(5,24,"%X     ",eax);
+	refresh();mvprintw(5,32,"%X     ",ebx);
+	refresh();mvprintw(5,40,"%X     ",ecx);
+	refresh();mvprintw(5,48,"%X     ",edx);
+
+	usleep(900000);
+}
+
+void imprimePCB(char archi[],int pidpcb){
+	int renglon=12;
+	mvprintw(10,2,"-----------------------PCB------------------------");
+	mvprintw(11,2,"PID\tArch\tEstado\tPC\tIR\tEAX\tEBX\tEDX\tECX\n");	
+		mvprintw(renglon,1,"                                                                                                                       ");
+		refresh();mvprintw(renglon+pidpcb,3,"%d",iniciolistos->actualPID=pidpcb);
+		refresh();mvprintw(renglon+pidpcb,8,"%s",strcpy(iniciolistos->nombreArch,archi));
+		refresh();mvprintw(renglon+pidpcb,24,"%d",iniciolistos->pc=pc);
+		refresh();mvprintw(renglon+pidpcb,27,"%s",strcpy(iniciolistos->auxIR,auxIR));
+		refresh();mvprintw(renglon+pidpcb,42,"%X",iniciolistos->eax=eax);
+		refresh();mvprintw(renglon+pidpcb,49,"%X",iniciolistos->ebx=ebx);
+		refresh();mvprintw(renglon+pidpcb,56,"%X",iniciolistos->ecx=ecx);
+		refresh();mvprintw(renglon+pidpcb,64,"%X",iniciolistos->edx=edx);
+}
+
 /*funcion que inserta al final de la lista*/
-void insertaFinal(char *nameArchivo,int PID){
+void insertFinal(char *nameArchivo,int PID){
 	struct Nodolistos *temp, *temp2;
 	//Apartamos ram para nuevo nodo
 	temp = (struct Nodolistos *) malloc(sizeof(struct Nodolistos));
@@ -67,63 +136,67 @@ void insertaFinal(char *nameArchivo,int PID){
 	}
 }
 
-void moverFinal(){
+int kbhit(void){
+    struct timeval tv;
+    fd_set read_fd;
+    tv.tv_sec = 0;
+    tv.tv_usec = 0;
+    FD_ZERO(&read_fd);
+    FD_SET(0, &read_fd);
+
+    if (select(1, &read_fd, NULL, NULL, &tv) == -1)
+        return 0;
+    if (FD_ISSET(0, &read_fd))
+        return 1;
+    return 0;
+}
+
+int moveFinal(int pidmover){
 	struct Nodolistos *temp, *auxMover;//temp: recorre la lista, auxMover: se movera al final.
+	int encontrado=0;
 	temp = iniciolistos;
-	auxMover=NULL;
+	auxMover=iniciolistos;
 
-		while(temp!=NULL){
-
+		while(temp!=NULL && encontrado==0){
+			if (temp->actualPID==pidmover && temp->siguiente==NULL)
+			{
+				break;
+			}
 			temp=temp->siguiente;
-			if(temp ->siguiente!=NULL && auxMover==NULL){
-				auxMover=iniciolistos;
-			}
-			if(temp->siguiente==NULL && auxMover==NULL){
-				free(temp);
-			}
-			if(temp->siguiente==NULL && auxMover!=NULL){
-				iniciolistos=auxMover->siguiente;
-				temp->siguiente=auxMover;
-				auxMover->siguiente=NULL;
+			if(temp->siguiente==NULL){
+				encontrado=1;
+				if(temp->actualPID==pidmover){
+					temp=iniciolistos;
+				}else{
+					iniciolistos=auxMover->siguiente;
+					temp->siguiente=auxMover;
+					auxMover->siguiente=NULL;	
+				}
 			}
 		}
+		return encontrado;
 }
+
 
 void MuestraArch(){
 	struct Nodolistos *temp;
 	int i=1;
 	temp=iniciolistos;
 	while(temp != NULL){
-		printf("PIB de Archivos: %d\n", temp->actualPID);
+		printf("PID de Archivos: %d\n", temp->actualPID);
 		printf("nombreArchivo: %s\n",temp->nombreArchivo );
 		i++;
 		temp=temp->siguiente;
 	}
 }
 
-int dispatcher(){
-	struct Nodolistos *temp;
-	temp=iniciolistos;
-	if(temp==NULL){
-		printf("Lista de listos VACIA\n");
-	}else{
-		proceso(temp->nombreArchivo);
-	}
-}
 
-void saveContext(){
-PCB.pc =pc;
-PCB.eax=eax;
-PCB.ebx= ebx;
-PCB.ecx= ecx;
-PCB.edx= edx;
-}
 
-int proceso(char archi[]){
+int proceso(char archi[],int pidproceso){
 	eax=0,ebx=0,ecx=0,edx=0;
 	int quantum = 0;
 	FILE *archivo;
-
+	struct Nodolistos *pcbprocess;
 	char *instruccion;
 	char *registroextra;
 	char *registro;
@@ -138,76 +211,58 @@ int proceso(char archi[]){
 	/*delimitadores*/
 	char del[4]=", \n";
 	char del2[2]="\n";
-	activo = 1;	
+	int estado = 1;
+	strcpy(auxarchi,archi);
 
-	if((archivo = fopen(nombreArchivo,"r"))!=NULL){
-		//printf("archivo abierto correctamente\n\n");
+	if((archivo = fopen(archi,"r"))!=NULL){
 		while(!feof(archivo)){
 			fgets(IR,15,archivo);
-			if(strcmp(IR,"\n")==0){printf("tienes un renglon vacio,pc = %d\n",pc);exit(1);}//verifica si hay un renglon vacio
+			if(strcmp(IR,"\n")==0){printf("tienes un renglon vacio,pc = %d\n",pc);continue;}//verifica si hay un renglon vacio
 			else{
 				if(feof(archivo)){
-					
+					moveFinal(pidproceso);
 				}else{
 					strcpy(auxIR,IR);
 					strtok(auxIR,del2);
 					instruccion=strtok(IR,del);
 					if(strcmp(instruccion,"MOV")==0){
-						//printf("es MOV\n");
 						operacion=1;			
 					}else if(strcmp(instruccion,"ADD")==0){
-						//printf("es ADD\n");
 						operacion=2;
 					}else if(strcmp(instruccion,"SUB")==0){
-						//printf("es SUB\n");
 						operacion=3;
 					}else if(strcmp(instruccion,"MUL")==0){
-						//printf("es MUL\n");
 						operacion=4;
 					}else if(strcmp(instruccion,"DIV")==0){
-						//printf("es DIV\n");
 						operacion=5;
 					}else if(strcmp(instruccion,"INC")==0){
-						//printf("es INC\n");
 						operacion=6;
 					}else if(strcmp(instruccion,"DEC")==0){
-						//printf("es DEC\n");
 						operacion=7;
 					}else if(strcmp(instruccion,"END")==0){
-						//printf("es END\n");
 						operacion=8;
 					}
 					if(operacion==8){
-						//printf("adios\n");
-						estado= 0;
-						//exit(1);
-
+						estado=3;
+						//status(estado);
 					}else{
-			
-								registro=strtok(NULL,del);
-							
-							
-								//printf("%s\n",registro);
+								registro=strtok(NULL,del);	
+								
 								if(strcmp(registro,"EAX")==0){
-									//printf("es EAX\n");
 									operador1=eax;
 									bandera1=1;
 								}else if(strcmp(registro,"EBX")==0){
-									//printf("es EBX\n");
 									operador1=ebx;
 									bandera1=2;
 								}else if(strcmp(registro,"ECX")==0){
-									//printf("es ECX\n");
 									operador1=ecx;
 									bandera1=3;
 								}else if(strcmp(registro,"EDX")==0){
-									//printf("es EDX\n");
 									operador1=edx;
 									bandera1=4;
 								}
 
-								if(operacion==7 || operacion==6){
-									//registroextra= strtok(NULL,del);								
+								if(operacion==7 || operacion==6){								
 									if (strtok(NULL,del)){
 										printf("tienes un registro extra, pc =%d\n",pc);
 										return 0;
@@ -243,22 +298,19 @@ int proceso(char archi[]){
 									if (registroextra){
 										//printf("\n");
 										printf("tienes un error en la sintaxis, pc = %d\n",pc);
-										endwin();
+										//endwin();
 										//printf("pc =%d\n",pc);
+										break;	
 										return 0;
 									}	
 									//printf("%s\n",regconst);
 									if(strcmp(regconst,"EAX")==0){
-										//printf("es EAX\n");
 											operador2=eax;		
 									}else if(strcmp(regconst,"EBX")==0){
-										//printf("es EBX\n");
 											operador2=ebx;
 									}else if(strcmp(regconst,"ECX")==0){
-										//printf("es ECX\n");
 											operador2=ecx;
 									}else if(strcmp(regconst,"EDX")==0){
-										//printf("es EDX\n");
 											operador2=edx;
 									}else{
 										//operador2=atoi(regconst);
@@ -331,101 +383,66 @@ int proceso(char archi[]){
 									}
 								}							
 					
-					imprime(pc);
-					
+					}
 					pc++;
 					quantum++;
-					if(quantum%3==0){
-						//moverFinal();
-						saveContext();
+						/*****/
+							if(kbhit()){
+								pid++;
+								refresh();
+								mvscanw(2,5,"%s",nombreArchivo);
+								if(strcmp(nombreArchivo,"salir")==0){endwin();MuestraArch();exit(0);}
+								else insertFinal(nombreArchivo,pid);
+							}
+						/*****/
+
+					imprime(pc);
+
+					if (quantum%3==0){
+						//estado=2;
+						//saveContext();
+						imprimePCB(archi,pidproceso);
+						//status(estado);
+						//fclose(archivo);
+						goto etiqueta_1;
+						
 					}
-					refresh();
-				}
-			
-			}//fin else feof(archivo)
-		}//fin else strcmp
-	}//fin while
-	fclose(archivo);
+					//status(estado);
+				}//fin else feof(archivo)
+
+			}//fin else strcmp
+		}//fin while
+
+		fclose(archivo);
+		printf("archivorecrrado\n");
+		
 	}else{
 		printf("error al abrir");
 	}
-return 0;
+	//moveFinal(pidproceso);
+	etiqueta_1:
+	moveFinal(pidproceso);
+	dispatcher();
+return 0;	
 }
 
-
-void imprime(int pc){
-	
-	initscr();
-	mvprintw(1,3,"$");
-
-	mvprintw(3,2,"------------------------CPU------------------------");
-	mvprintw(4,2,"PC\tIR\t\tEAX\tEBX\tECX\tEDX\n");
-
-
-	mvprintw(10,2,"-----------------------PCB------------------------");
-	mvprintw(11,2,"PID\tEstado\t\tPC\tIR\tEAX\tEBX\tEDX\tECX\n");
-
-	refresh();mvprintw(5,2,"%d",pc);refresh();
-	refresh();mvprintw(5,8,"%s      ",auxIR);
-	refresh();mvprintw(5,24,"%X     ",eax);
-	refresh();mvprintw(5,32,"%X     ",ebx);
-	refresh();mvprintw(5,40,"%X     ",ecx);
-	refresh();mvprintw(5,48,"%X     ",edx);
-	mvprintw(12,24,"%d",PCB.pc);
-	mvprintw(12,40,"%X",PCB.eax);
-	mvprintw(12,48,"%X",PCB.ebx);
-	mvprintw(12,56,"%X",PCB.ecx);
-	mvprintw(12,64,"%X",PCB.edx);
-	usleep(700000);
-	
-		if(kbhit()){
-			refresh();
-			mvscanw(2,5,"%s",nombreArchivo);
-			if(strcmp(nombreArchivo,"salir")==0){
-				endwin();
-				//MuestraArch();
-				exit(0);
-			}else{
-				pid++;
-				//saveContext();
-				insertaFinal(nombreArchivo,pid);
-				moverFinal();
-				dispatcher();
-			}
-			//MuestraArch();
-		}
-
+void readArch(){
+		refresh();	
+		mvprintw(2,3,"$");
+		mvscanw(2,5,"%s",nombreArchivo);
+		insertFinal(nombreArchivo,pid);
+		refresh();
+		dispatcher();	
 }
 
-int kbhit(void) 
-{
-    struct timeval tv;
-    fd_set read_fd;
-    tv.tv_sec = 0;
-    tv.tv_usec = 0;
-    FD_ZERO(&read_fd);
-    FD_SET(0, &read_fd);
-
-    if (select(1, &read_fd, NULL, NULL, &tv) == -1)
-        return 0;
-
-    if (FD_ISSET(0, &read_fd))
-        return 1;
-
-    return 0;
-
-}
+/*++++FIN FUNCIONES++++*/
 
 int  main(void){
 	//declaracion cabezera de listos
 	iniciolistos=NULL;
 	initscr();
-	mvprintw(2,3,"$");
-	mvscanw(2,5,"%s",nombreArchivo);
-	insertaFinal(nombreArchivo,pid);
 	refresh();
-	dispatcher();
-
+	readArch();
 	endwin();
 return 0;
 }
